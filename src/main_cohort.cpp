599fd4ad56cd6d158a921d770f8a7afd28023fea
Alexander Pletzer <alexander@gokliya.net>
2025-07-31 16:31:41 +1200

    working on a single instance

--- src/main_cohort.cpp
+++ src/main_cohort.cpp
@@ -1,7 +1,6 @@
 #include <iostream>
 #include <cstring>
 #include <cstdlib>
-#include <mpi.h>
 #include "SeapodymCohort.h"
 #include <CmdLineArgParser.h>
 
@@ -14,30 +13,25 @@ int main(int argc, char** argv) {
 
 	// Initialization of MPI
 	int err;
-	err = MPI_Init(&argc, &argv);
 	int workerId = 0;
-	err = MPI_Comm_rank(MPI_COMM_WORLD, &workerId);
-	int num_workers = 1;
-	err = MPI_Comm_size(MPI_COMM_WORLD, &num_workers);
-	
+	int num_workers = 1;	
 	
 	int cmp_regime = 0;
 	bool reset_buffers = false;
 
 	CmdLineArgParser cmdLine;
 	cmdLine.set("-s", std::string("initparfile.xml"), "Input parameter file");
-	cmdLine.set("-na", 1, "Number of age groups");
 	// Parse the command line arguments
     bool success = cmdLine.parse(argc, argv);
     bool help = cmdLine.get<bool>("-help") || cmdLine.get<bool>("-h");
     if (!success) {
         std::cerr << "Error parsing command line arguments." << std::endl;
         cmdLine.help();
-        MPI_Abort(MPI_COMM_WORLD, 1);
+        exit(1);
     }
     if (help) {
         cmdLine.help();
-        MPI_Abort(MPI_COMM_WORLD, 1);
+        exit(1);
     }
 
 	//-----Memory stack sizes for dvariables and derivatives storage------
@@ -52,62 +46,17 @@ int main(int argc, char** argv) {
 	gradient_structure::set_GRADSTACK_BUFFER_SIZE(gradstack_buffer);
 	gradient_structure::set_CMPDIF_BUFFER_SIZE(cmpdif_buffer);
 	gradient_structure gs(gs_var_buffer);
-
-	int num_age_groups = cmdLine.get<int>("-na");
-	std::vector<int> cohort_ids;
-	for (int ia = 0; ia < num_age_groups; ++ia) {
-		if (ia % num_workers == workerId) {
-			cohort_ids.push_back(ia);
-		}
-	}
-
-	// cohorts handled by this worker
-	std::vector< SeapodymCohort* > cohorts;
-	const char* parfile = cmdLine.get<std::string>("-s").c_str();
-
-	// Initialize the cohorts for each age group assigned to this worker
 	int out_hessian = 0;
 	gradient_structure::set_USE_FOR_HESSIAN(out_hessian);
 
-	// //initialize variables of optimization
-	// VarParamCoupled var;
-	// var.read(parfile);
-	// const int nvar = var.nvarcalc();
-	// independent_variables x(1, nvar);
-	// adstring_array x_names(1, nvar);
+	// cohorts handled by this worker
+	const char* parfile = cmdLine.get<std::string>("-s").c_str();
 
-	for (auto cohort_id : cohort_ids) {
-
-		SeapodymCohort* scp = seapodym_cohort(parfile, cmp_regime, reset_buffers, cohort_id, gs);
-
-		// //read parfile
-		// SeapodymCohort* scp = new SeapodymCohort(parfile, cohort_id);
-		// SeapodymCohort& sc = *scp;
-
-		// sc.xinit(x, x_names);
-		// cout << "Total number of variables: " << nvar << '\n'<<'\n';
-
-		// //initialization of simulation
-		// sc.prerun_model();
-
-		cohorts.push_back(scp);
-	}
-
-	// TO DO, run a single step for each cohort habdled by this worker
-	for (auto scp : cohorts) {
-		//scp->prerun_model();
-		scp->OnRunFirstStep();
-		// scp->stepForward(false); // false means no output files written
-	}
-
-	// Clean up 
-	for (auto scp : cohorts) {
-		delete scp;
-	}
-
-	// Finalization of MPI
-	////////////////////////////////////////////////////////////////////////
-	err = MPI_Finalize();
+	// Initialize the cohorts for each age group assigned to this worker
+	int cohort_id = 0;
+	SeapodymCohort* scp = seapodym_cohort(parfile, cmp_regime, reset_buffers, cohort_id, gs);
+	scp->OnRunFirstStep();
+	delete scp;
 
 	return 0;
 }
